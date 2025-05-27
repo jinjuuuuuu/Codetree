c83d16def91f672fccf93200b9430125f59db6c2
Jinju <143995943+jinjuuuuuu@users.noreply.github.com>
2025-05-27 12:51:18 +0900

    Title: 최대로 겹치는 구간, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250527/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250527/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int[] x1 = new int[n];
         int[] x2 = new int[n];
@@ -14,24 +13,29 @@ public class Main {
         for (int i = 0; i < n; i++) {
             x1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
-            min = Math.min(min, Math.min(x1[i], x2[i]));
-            max = Math.max(max, Math.max(x1[i], x2[i]));
+            min = Math.min(min, x1[i]);
+            min = Math.min(min, x2[i]);
+            max = Math.max(max, x1[i]);
+            max = Math.max(max, x2[i]);
         }
 
-        int offset = -min;
-        int[] arr = new int[max - min + 2];
+        int offset = -min + 1;
+        int size = max - min + 3;
+        int[] arr = new int[size];
 
         for (int i = 0; i < n; i++) {
             int start = x1[i] + offset;
             int end = x2[i] + offset;
-            for (int j = start; j <= end; j++) {
-                arr[j]++;
-            }
+            arr[start] += 1;
+            arr[end] -= 1;
         }
 
         int result = 0;
-        for (int cnt : arr) {
-            if (cnt > result) result = cnt;
+        int sum = 0;
+
+        for (int i = 0; i < size; i++) {
+            sum += arr[i];
+            result = Math.max(result, sum);
         }
 
         System.out.println(result);
