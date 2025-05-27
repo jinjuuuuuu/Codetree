8d8452e96ab088c406332045d5cd8de3142112ab
Jinju <143995943+jinjuuuuuu@users.noreply.github.com>
2025-05-27 12:45:44 +0900

    Title: 최대로 겹치는 구간, Time: 145ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250527/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250527/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,23 +1,39 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int[] arr = new int[1001];
-        int max = -1;
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
         for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            for (int j = a; j <= b; j++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            min = Math.min(min, Math.min(x1[i], x2[i]));
+            max = Math.max(max, Math.max(x1[i], x2[i]));
+        }
+
+        int offset = -min;
+        int[] arr = new int[max - min + 2];
+
+        for (int i = 0; i < n; i++) {
+            int start = x1[i] + offset;
+            int end = x2[i] + offset;
+            for (int j = start; j <= end; j++) {
                 arr[j]++;
             }
         }
 
-        for (int i = 0; i < 100; i++) {
-            if (arr[i] > max) max = arr[i];
+        int result = 0;
+        for (int cnt : arr) {
+            if (cnt > result) result = cnt;
         }
 
-        System.out.println(max);
-        // Please write your code here.
+        System.out.println(result);
     }
 }
